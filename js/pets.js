4c811b6c78e72ff35b4e8e665b6d71ad1e51d891
viewtifult <tashanduncan1994@gmail.com>
2020-11-07 18:49:45 +0000

    create array of pets

--- js/pets.js
+++ js/pets.js
@@ -3,3 +3,41 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
+const pets = [
+{ name:'Liam',
+  type:'dog',
+  breed:'aussie',
+  age:'28',
+  photo: 'aussie.jpg'
+},
+{ name:'Mekhi',
+  type:'dog',
+  breed:'dachshund',
+  age:'10',
+  photo: 'dachshund.jpg'
+}
+{ name:'Will',
+  type:'dog',
+  breed:'golden',
+  age:'28',
+  photo: 'golden.jpg'
+},
+{ name:'Kamal',
+  type:'cat',
+  breed:'persian',
+  age:'20',
+  photo: 'persian.jpg'
+}
+{ name:'Tashan',
+  type:'dog',
+  breed:'pug',
+  age:'26',
+  photo: 'pug.jpg'
+},
+{ name:'Angela',
+  type:'cat',
+  breed:'tabby',
+  age:'54',
+  photo: 'tabby.jpg'
+}
+]
