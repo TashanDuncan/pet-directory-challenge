2adf052c6c8fc1c023cf528293017dde551eb734
viewtifult <tashanduncan1994@gmail.com>
2020-11-07 19:25:37 +0000

    complete challenge but using for loot to itterate through array of objects

--- js/pets.js
+++ js/pets.js
@@ -8,36 +8,45 @@ const pets = [
   type:'dog',
   breed:'aussie',
   age:'28',
-  photo: 'aussie.jpg'
+  photo: 'img/aussie.jpg'
 },
 { name:'Mekhi',
   type:'dog',
   breed:'dachshund',
   age:'10',
-  photo: 'dachshund.jpg'
-}
+  photo: 'img/dachshund.jpg'
+},
 { name:'Will',
   type:'dog',
   breed:'golden',
   age:'28',
-  photo: 'golden.jpg'
+  photo: 'img/golden.jpg'
 },
 { name:'Kamal',
   type:'cat',
   breed:'persian',
   age:'20',
-  photo: 'persian.jpg'
-}
+  photo: 'img/persian.jpg'
+},
 { name:'Tashan',
   type:'dog',
   breed:'pug',
   age:'26',
-  photo: 'pug.jpg'
+  photo: 'img/pug.jpg'
 },
 { name:'Angela',
   type:'cat',
   breed:'tabby',
   age:'54',
-  photo: 'tabby.jpg'
+  photo: 'img/tabby.jpg'
 }
 ]
+
+for (let i = 0; i < pets.length; i++){
+  document.querySelector('main').innerHTML += `
+  <h2>${pets[i].name}</h2>
+  <h3>${pets[i].type} | ${pets[i].breed}</h3>
+  <p>Age: ${pets[i].age}</p>
+  <img src="${pets[i].photo}" alt="${pets[i].breed}">
+  ` 
+}
